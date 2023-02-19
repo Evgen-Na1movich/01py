1c6f4dc3b9b92074be3f44cd67d93701984728c3
Evgen Naimovich <evgenii.blr.88@gmail.com>
2023-02-19 16:42:11 +0300

    first commit

---
+++ PycharmProjects/pythonProject1/lesson/lesson2/vizitka.py
@@ -0,0 +1,6 @@
+name = input("Ваше имя:\n ")
+age = input("Сколько вам лет?\n")
+num_grup = input("Какой номер твоей группы?\n")
+city = input("Откуда ты?\n")
+print(f"Привет. Меня зовут {name}, я изучаю язык программировани'Python' в IT-школе 'TeachMeSkills', в группе {num_grup}."
+      f" Мне {age}  живу в {city}e")
